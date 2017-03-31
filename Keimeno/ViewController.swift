d239b1469bbde9726ad4697f82b37de063946349
Pranav Madanahalli <drumin99@gmail.com>
2017-03-31 14:46:12 -0700

    Integrated photo upload from camera and library

--- Keimeno/ViewController.swift
+++ Keimeno/ViewController.swift
@@ -10,40 +10,70 @@ import UIKit
 import TesseractOCR
 import TextToSpeechV1
 import AVFoundation
+import ALCameraViewController
 
 class ViewController: UIViewController, G8TesseractDelegate {
 
-    @IBOutlet var textView: UITextView!
     var audioPlayer: AVAudioPlayer!
+    var imageGlobal: UIImage!
+    
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       if let tesseract = G8Tesseract(language: "eng"){
         
+        
+        
+
+    }
+    @IBAction func playAudio(_ sender: Any) {
+        
+        if let tesseract = G8Tesseract(language: "eng"){
+            
             tesseract.delegate = self
-            tesseract.image = UIImage(named: "demoText")?.g8_blackAndWhite()
-        
+            tesseract.image = imageView.image?.g8_blackAndWhite()
+            
             tesseract.recognize()
-            textView.text = tesseract.recognizedText
+            //textView.text = tesseract.recognizedText
             let username = "0323dd98-eb02-4868-8a36-b78ee9640ce2"
             let password = "cgEKeaPkWKQj"
             let textToSpeech = TextToSpeech(username: username, password: password)
-        
-            let text = tesseract.recognizedText
+            
+            print("\(tesseract.recognizedText)")
+            let text = tesseract.recognizedText as String
             let failure = { (error: Error) in print(error) }
-            textToSpeech.synthesize(text!, failure: failure) { data in
+            textToSpeech.synthesize(text, failure: failure) { data in
                 self.audioPlayer = try! AVAudioPlayer(data: data)
                 self.audioPlayer.play()
             }
-        
+            
         }
         
-       
+    }
+    @IBAction func openCamera(_ sender: Any) {
         
+        let cameraViewController = CameraViewController(croppingEnabled: true, allowsLibraryAccess: true) { [weak self] image, asset in
+            self?.imageView.image = image
+            self?.dismiss(animated: true, completion: nil)
+        }
+        
+        
+        present(cameraViewController, animated: true, completion: nil)
         
         
         
     }
+    @IBAction func openLibrary(_ sender: Any) {
+        let libraryViewController = CameraViewController.imagePickerViewController(croppingEnabled: true) { image, asset in
+            self.imageView.image = image
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        present(libraryViewController, animated: true, completion: nil)
+    }
+    
+    
     func progressImageRecognition(for tesseract: G8Tesseract!){
         
         print("Recognitiion Progress \(tesseract.progress)%")
