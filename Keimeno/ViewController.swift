8bd3b640974d227ee319e8b334a1a7fc5d96e437
Pranav Madanahalli <drumin99@gmail.com>
2017-03-31 13:01:34 -0700

    Integrated Google's Tesseract OCR for scaring photos for text. Integrated IBM Watson's Text to Speech.

--- Keimeno/ViewController.swift
+++ Keimeno/ViewController.swift
@@ -7,13 +7,49 @@
 //
 
 import UIKit
+import TesseractOCR
+import TextToSpeechV1
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, G8TesseractDelegate {
 
+    @IBOutlet var textView: UITextView!
+    var audioPlayer: AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       if let tesseract = G8Tesseract(language: "eng"){
+        
+            tesseract.delegate = self
+            tesseract.image = UIImage(named: "demoText")?.g8_blackAndWhite()
+        
+            tesseract.recognize()
+            textView.text = tesseract.recognizedText
+            let username = "0323dd98-eb02-4868-8a36-b78ee9640ce2"
+            let password = "cgEKeaPkWKQj"
+            let textToSpeech = TextToSpeech(username: username, password: password)
+        
+            let text = tesseract.recognizedText
+            let failure = { (error: Error) in print(error) }
+            textToSpeech.synthesize(text!, failure: failure) { data in
+                self.audioPlayer = try! AVAudioPlayer(data: data)
+                self.audioPlayer.play()
+            }
+        
+        }
+        
+       
+        
+        
+        
+        
     }
+    func progressImageRecognition(for tesseract: G8Tesseract!){
+        
+        print("Recognitiion Progress \(tesseract.progress)%")
+    }
+
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
