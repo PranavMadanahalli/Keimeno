850f6477c87fef7235a23262adad19bbd3c5a2df
Pranav Madanahalli <drumin99@gmail.com>
2017-04-07 13:04:18 -0700

    added feature that allows users to take multiple pictures which adds to their audio book!

--- Keimeno/ViewController.swift
+++ Keimeno/ViewController.swift
@@ -12,50 +12,56 @@ import TextToSpeechV1
 import AVFoundation
 import ALCameraViewController
 
-class ViewController: UIViewController, G8TesseractDelegate {
+class ViewController: UIViewController, UITextViewDelegate, G8TesseractDelegate {
 
     var audioPlayer: AVAudioPlayer!
     var imageGlobal: UIImage!
     
+    @IBOutlet var textView: UITextView!
     @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
+        self.textView.delegate = self;
+
+        textView.text = " "
         
 
     }
     @IBAction func playAudio(_ sender: Any) {
         
-        if let tesseract = G8Tesseract(language: "eng"){
-            
-            tesseract.delegate = self
-            tesseract.image = imageView.image?.g8_blackAndWhite()
-            
-            tesseract.recognize()
-            //textView.text = tesseract.recognizedText
-            let username = "0323dd98-eb02-4868-8a36-b78ee9640ce2"
-            let password = "cgEKeaPkWKQj"
-            let textToSpeech = TextToSpeech(username: username, password: password)
-            
-            print("\(tesseract.recognizedText)")
-            let text = tesseract.recognizedText as String
-            let failure = { (error: Error) in print(error) }
-            textToSpeech.synthesize(text, failure: failure) { data in
-                self.audioPlayer = try! AVAudioPlayer(data: data)
-                self.audioPlayer.play()
-            }
-            
-        }
         
+        let username = "0323dd98-eb02-4868-8a36-b78ee9640ce2"
+        let password = "cgEKeaPkWKQj"
+        let textToSpeech = TextToSpeech(username: username, password: password)
+        
+        print("\(textView.text)")
+        let failure = { (error: Error) in print(error) }
+        textToSpeech.synthesize(textView.text, failure: failure) { data in
+            self.audioPlayer = try! AVAudioPlayer(data: data)
+            self.audioPlayer.play()
+        }
+
+    }
+    @IBAction func clearTextButton(_ sender: Any) {
+        
+        textView.text = " "
     }
     @IBAction func openCamera(_ sender: Any) {
         
-        let cameraViewController = CameraViewController(croppingEnabled: true, allowsLibraryAccess: true) { [weak self] image, asset in
+        let cameraViewController = CameraViewController(croppingEnabled: false, allowsLibraryAccess: true) { [weak self] image, asset in
             self?.imageView.image = image
             self?.dismiss(animated: true, completion: nil)
+
+            if let tesseract = G8Tesseract(language: "eng"){
+                
+                tesseract.delegate = self
+                tesseract.image = self?.imageView.image?.g8_blackAndWhite()
+                
+                tesseract.recognize()
+                self?.textView.text = (self?.textView.text)! + tesseract.recognizedText
+                
+            }
         }
         
         
@@ -65,10 +71,11 @@ class ViewController: UIViewController, G8TesseractDelegate {
         
     }
     @IBAction func openLibrary(_ sender: Any) {
-        let libraryViewController = CameraViewController.imagePickerViewController(croppingEnabled: true) { image, asset in
+        let libraryViewController = CameraViewController.imagePickerViewController(croppingEnabled: false) { image, asset in
             self.imageView.image = image
             self.dismiss(animated: true, completion: nil)
         }
+        
         
         present(libraryViewController, animated: true, completion: nil)
     }
@@ -77,6 +84,13 @@ class ViewController: UIViewController, G8TesseractDelegate {
     func progressImageRecognition(for tesseract: G8Tesseract!){
         
         print("Recognitiion Progress \(tesseract.progress)%")
+    }
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
     }
 
     
